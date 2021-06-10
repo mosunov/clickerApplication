3021bfb107cf3ee4ccef6cc902278f4b9ffbdd47
Mosunov <leonid_mosunov@mail.ru>
2021-06-11 01:17:52 +0300

    add methods in ClickService add realization of IncrementService

--- src/main/java/ru/task/clickerapplication/service/ClickService.java
+++ src/main/java/ru/task/clickerapplication/service/ClickService.java
@@ -1,4 +1,10 @@
 package ru.task.clickerapplication.service;
 
+import com.sun.istack.NotNull;
+
 public interface ClickService {
+    @NotNull
+    String getCounter();
+
+    @NotNull String increment();
 }
--- src/main/java/ru/task/clickerapplication/service/IncrementService.java
+++ src/main/java/ru/task/clickerapplication/service/IncrementService.java
@@ -1,4 +1,48 @@
 package ru.task.clickerapplication.service;
 
-public class IncrementService {
+
+import com.sun.istack.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.task.clickerapplication.repository.ClickerEntity;
+import ru.task.clickerapplication.repository.ClickerRepository;
+
+import java.math.BigInteger;
+
+@Service
+@RequiredArgsConstructor
+public class IncrementService implements ClickService {
+
+    private static final Long COUNTER_ID = 1L;
+    private final ClickerRepository repository;
+
+    @Transactional
+    @NotNull
+    @Override
+    public String getCounter() {
+        return findOrCreate().getCounter();
+    }
+
+    @Transactional
+    @NotNull
+    @Override
+    public String increment() {
+        ClickerEntity clickHolder = findOrCreate();
+        String newValue = new BigInteger(clickHolder.getCounter()).add(BigInteger.ONE).toString();
+        clickHolder.setCounter(newValue);
+        return repository.save(clickHolder).getCounter();
+    }
+
+    private ClickerEntity findOrCreate() {
+        return repository.findById(COUNTER_ID).orElseGet(this::createCounter);
+    }
+
+    private ClickerEntity createCounter() {
+        ClickerEntity clicker = new ClickerEntity();
+        clicker.setId(COUNTER_ID);
+        clicker.setCounter(BigInteger.ZERO.toString());
+        return repository.save(clicker);
+    }
+
 }
