c5580a471d3e82263d8599f978ee777ace58921e
Mosunov <leonid_mosunov@mail.ru>
2021-06-11 01:10:45 +0300

    add database entity and extension of ClickerRepository interface

--- src/main/java/ru/task/clickerapplication/repository/ClickerEntity.java
+++ src/main/java/ru/task/clickerapplication/repository/ClickerEntity.java
@@ -1,4 +1,22 @@
 package ru.task.clickerapplication.repository;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "test")
 public class ClickerEntity {
-}
+        @Id
+        private Long id;
+        private String counter;
+
+        public ClickerEntity(String counter) {
+            this.counter = counter;
+        }
+
+        public ClickerEntity() {}
+    }
--- src/main/java/ru/task/clickerapplication/repository/ClickerRepository.java
+++ src/main/java/ru/task/clickerapplication/repository/ClickerRepository.java
@@ -1,4 +1,8 @@
 package ru.task.clickerapplication.repository;
 
-public interface ClickerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClickerRepository extends JpaRepository<ClickerEntity,Long> {
 }
