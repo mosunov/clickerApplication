579e511597b5d704f29652bb0e6b49b58ae23242
Mosunov <leonid_mosunov@mail.ru>
2021-06-11 01:23:30 +0300

    add controller realization and put settings for connection to database in application.properties

--- src/main/java/ru/task/clickerapplication/controller/Controller.java
+++ src/main/java/ru/task/clickerapplication/controller/Controller.java
@@ -1,4 +1,24 @@
 package ru.task.clickerapplication.controller;
 
-public class Controller {
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.task.clickerapplication.service.ClickService;
+
+@RestController
+@RequiredArgsConstructor
+public class Controller{
+
+    private final ClickService clickService;
+
+    @PostMapping(value = "/index")
+    public String incrementCounter() {
+        return clickService.increment();
+    }
+
+    @GetMapping(value ="/index")
+    public String getCounter() {
+        return clickService.getCounter();
+    }
 }
